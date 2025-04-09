2492d6096d7e175b926000195c71bf736cad710f
riyajatar37003 <riyaj.atar@servicenow.com>
2025-04-09 16:29:50 +0530

    Update modeling.py

--- Reranker/src/modeling.py
+++ Reranker/src/modeling.py
@@ -2,21 +2,17 @@ import logging
 
 import torch
 from torch import nn
-from transformers import AutoModelForSequenceClassification, PreTrainedModel, TrainingArguments,AutoTokenizer,AutoModel
+from transformers import AutoModelForSequenceClassification, PreTrainedModel, TrainingArguments
 from transformers.modeling_outputs import SequenceClassifierOutput
 
 from .arguments import ModelArguments, DataArguments
 
 logger = logging.getLogger(__name__)
-import torch.nn.functional as F
+
 
 class CrossEncoder(nn.Module):
     def __init__(self, hf_model: PreTrainedModel, model_args: ModelArguments, data_args: DataArguments,
-                 train_args: TrainingArguments,
-                kd_weight:float ,
-                teacher_model_path:str
-                student_tokenizer_path:str):
-        
+                 train_args: TrainingArguments):
         super().__init__()
         self.hf_model = hf_model
         self.model_args = model_args
@@ -25,15 +21,7 @@ class CrossEncoder(nn.Module):
 
         self.config = self.hf_model.config
         self.cross_entropy = nn.CrossEntropyLoss(reduction='mean')
-        self.margin_loss =  nn.MultiMarginLoss(margin=1.0)
-        self.kld_loss = nn.KLDivLoss(reduction='batchmean',log_target=True)
-        self.kd_weight = kd_weight
-        
-        self.teacher_tokenizer = AutoTokenizer.from_pretrained(t_path,trust_remote_code=True )
-        self.student_tokenizer = AutoTokenizer.from_pretrained(s_path,trust_remote_code=True )
-        
-        self.teacher_model = AutoModelForSequenceClassification.from_pretrained(t_path,trust_remote_code=True).to('cuda')
-        
+
         self.register_buffer(
             'target_label',
             torch.zeros(self.train_args.per_device_train_batch_size, dtype=torch.long)
@@ -43,38 +31,16 @@ class CrossEncoder(nn.Module):
         self.hf_model.gradient_checkpointing_enable(**kwargs)
 
     def forward(self, batch):
-        
-        tt = self.student_tokenizer.batch_decode(batch['input_ids'],skip_special_tokens=True)
-        t_ids = self.teacher_tokenizer(tt,return_tensors='pt',padding=True,truncation=True).to('cuda')
-        
-        with torch.no_grad():
-            
-            t_output = self.teacher_model(
-                **t_ids, return_dict=True, output_hidden_states=True
-            )
-            t_logits = t_output.logits.view(self.train_args.per_device_train_batch_size,self.data_args.train_group_size)
-            
         ranker_out: SequenceClassifierOutput = self.hf_model(**batch, return_dict=True)
-        
         logits = ranker_out.logits
-        
 
-        
         if self.training:
             scores = logits.view(
                 self.train_args.per_device_train_batch_size,
                 self.data_args.train_group_size
-            ) # batch_size x number of [pos+neg]
-            # correct class label is present at zero'th index.
-            s_logits =  F.log_softmax(scores, dim=1)
-            t_logits =  F.log_softmax(t_logits, dim=1)
-            
-            loss1 = self.kld_loss(s_logits, t_logits)
-            loss2 = self.cross_entropy(scores, self.target_label)
-            # loss2 = self.margin_loss(scores, self.target_label)
-            
-            loss = (1-self.kd_weight)*loss1 + self.kd_weight*loss2
-            
+            )
+            loss = self.cross_entropy(scores, self.target_label)
+
             return SequenceClassifierOutput(
                 loss=loss,
                 **ranker_out,
